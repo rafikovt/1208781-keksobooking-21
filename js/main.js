d42a4777abc274f7771fd3cc2259dff98631677b
Timur <rafegg21@gmail.com>
2020-09-24 20:24:44 +0500

    Изменения после консультации

--- js/main.js
+++ js/main.js
@@ -2,19 +2,24 @@
 const TYPES = [`palace`, `flat`, `house`, `bungalow`];
 const TIMES = [`12:00`, `13:00`, `14:00`];
 const FEATURES = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
-const PHOTOS = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`];
+const PHOTOS = [
+  `http://o0.github.io/assets/images/tokyo/hotel1.jpg`,
+  `http://o0.github.io/assets/images/tokyo/hotel2.jpg`,
+  `http://o0.github.io/assets/images/tokyo/hotel3.jpg`];
 const COUNT_OF_MOCK = 8;
 const LOCATION_X_MIN = 10;
 const LOCATION_X_MAX = 1000;
 const LOCATION_Y_MIN = 130;
 const LOCATION_Y_MAX = 630;
+const NUMBER_OF_CARD = 0;
 const map = document.querySelector(`.map`);
-const pinsContainer = document.querySelector(`.map__pins`);
+const pinsContainer = map.querySelector(`.map__pins`);
 const newPinTemplate = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
+
 const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (Math.floor(max) - min)) + min;
 };
-const getRandomElement = (arr) => arr[Math.floor(Math.random() * arr.length)];
+const getRandomElement = (arr) => arr[getRandomInt(0, arr.length - 1)];
 const getRandomLengthArr = (arr) => {
   const newArr = [];
   for (let i = 0; i < getRandomInt(1, arr.length); i++) {
@@ -24,35 +29,35 @@ const getRandomLengthArr = (arr) => {
 };
 const generateMoks = (countOfMocks) => {
   const moks = [];
-  for (let i = 0; i < countOfMocks; i++) {
-    moks[i] = {
+  for (let i = 1; i <= countOfMocks; i++) {
+    moks.push({
       author: {
-        avatar: `img/avatars/user0${getRandomInt(1, 8)}.png`
+        avatar: `img/avatars/user0${i}.png`
       },
       offer: {
         title: `Заголовок объявления`,
         address: `${getRandomInt(LOCATION_X_MIN, LOCATION_X_MAX)}, ${getRandomInt(LOCATION_Y_MIN, LOCATION_Y_MAX)}`,
-        price: null,
+        price: getRandomInt(100, 5000),
         type: getRandomElement(TYPES),
-        rooms: null,
-        guests: null,
+        rooms: getRandomInt(1, 5),
+        guests: getRandomInt(1, 6),
         checkin: getRandomElement(TIMES),
         checkout: getRandomElement(TIMES),
         features: getRandomLengthArr(FEATURES),
-        description: ``,
+        description: `Великолепная квартира в центре Токио`,
         photos: getRandomLengthArr(PHOTOS)
       },
       location: {
         x: getRandomInt(LOCATION_X_MIN, LOCATION_X_MAX),
         y: getRandomInt(LOCATION_Y_MIN, LOCATION_Y_MAX)
       }
-    };
+    });
   }
   return moks;
 };
 const mocks = generateMoks(COUNT_OF_MOCK);
 map.classList.remove(`map--faded`);
-const createPins = (moks) => {
+const createPin = (moks) => {
   const newPin = newPinTemplate.cloneNode(true);
   const newPinImg = newPin.querySelector(`img`);
   newPin.style = `left: ${moks.location.x - newPinImg.width}px; 
@@ -64,8 +69,32 @@ const createPins = (moks) => {
 const renderPins = (moks) => {
   const fragment = document.createDocumentFragment();
   for (let i = 0; i < COUNT_OF_MOCK; i++) {
-    fragment.appendChild(createPins(moks[i]));
+    fragment.appendChild(createPin(moks[i]));
   }
   pinsContainer.appendChild(fragment);
 };
 renderPins(mocks);
+
+
+const cardTemplate = document.querySelector(`#card`).content.querySelector(`.map__card`);
+const cardsContainer = document.querySelector(`.map`);
+
+const createCards = (numberOfCard) => {
+  const newCard = cardTemplate.cloneNode(true);
+  newCard.querySelector(`.popup__title`).textContent = `${mocks[numberOfCard].offer.title}`;
+  newCard.querySelector(`.popup__text--address`).textContent = `${mocks[numberOfCard].offer.address}`;
+  newCard.querySelector(`.popup__text--price`).textContent = `${mocks[numberOfCard].offer.price}₽/ночь`;
+  newCard.querySelector(`.popup__text--capacity`).textContent = `${mocks[numberOfCard].offer.rooms} комнаты для ${mocks[0].offer.guests} гостей`;
+  newCard.querySelector(`.popup__text--time`).textContent = `Заезд после ${mocks[numberOfCard].offer.checkin}, выезд до ${mocks[0].offer.checkout}`;
+  const popupFeatures = Array.from(newCard.querySelectorAll(`.popup__feature`));
+  const popupFeaturesContainer = newCard.querySelector(`.popup__features`);
+  const availableFeatures = popupFeatures.filter((elem) => !mocks[numberOfCard].offer.features.some((str) => elem.className.includes(`--${str}`)));
+  availableFeatures.forEach((feature) => popupFeaturesContainer.removeChild(feature));
+  return newCard;
+};
+const renderCards = () => {
+  const fragment = document.createDocumentFragment();
+  fragment.appendChild(createCards(NUMBER_OF_CARD));
+  cardsContainer.insertBefore(fragment, document.querySelector(`.map__filters-container`));
+};
+renderCards();
