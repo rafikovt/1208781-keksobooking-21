1bcdc5ca33c77f9c4d3a90254d7815c087f65088
Timur <rafegg21@gmail.com>
2020-09-25 18:47:58 +0500

    module3-task2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 'use strict';
 const TYPES = [`palace`, `flat`, `house`, `bungalow`];
+const TYPES_TRANSLATE = {
+  palace: `Дворец`,
+  flat: `Квартира`,
+  house: `Дом`,
+  bungalow: `Дворец`
+};
 const TIMES = [`12:00`, `13:00`, `14:00`];
 const FEATURES = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
 const PHOTOS = [
@@ -15,10 +21,10 @@ const NUMBER_OF_CARD = 0;
 const map = document.querySelector(`.map`);
 const pinsContainer = map.querySelector(`.map__pins`);
 const newPinTemplate = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
+const cardTemplate = document.querySelector(`#card`).content.querySelector(`.map__card`);
+const cardsContainer = document.querySelector(`.map`);
 
-const getRandomInt = (min, max) => {
-  return Math.floor(Math.random() * (Math.floor(max) - min)) + min;
-};
+const getRandomInt = (min, max) => Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1)) + Math.ceil(min);
 const getRandomElement = (arr) => arr[getRandomInt(0, arr.length - 1)];
 const getRandomLengthArr = (arr) => {
   const newArr = [];
@@ -75,26 +81,38 @@ const renderPins = (moks) => {
 };
 renderPins(mocks);
 
+// module3-task2
 
-const cardTemplate = document.querySelector(`#card`).content.querySelector(`.map__card`);
-const cardsContainer = document.querySelector(`.map`);
 
-const createCards = (numberOfCard) => {
+const createCard = (numberOfMock) => {
   const newCard = cardTemplate.cloneNode(true);
-  newCard.querySelector(`.popup__title`).textContent = `${mocks[numberOfCard].offer.title}`;
-  newCard.querySelector(`.popup__text--address`).textContent = `${mocks[numberOfCard].offer.address}`;
-  newCard.querySelector(`.popup__text--price`).textContent = `${mocks[numberOfCard].offer.price}₽/ночь`;
-  newCard.querySelector(`.popup__text--capacity`).textContent = `${mocks[numberOfCard].offer.rooms} комнаты для ${mocks[0].offer.guests} гостей`;
-  newCard.querySelector(`.popup__text--time`).textContent = `Заезд после ${mocks[numberOfCard].offer.checkin}, выезд до ${mocks[0].offer.checkout}`;
+  newCard.querySelector(`.popup__title`).textContent = `${mocks[numberOfMock].offer.title}`;
+  newCard.querySelector(`.popup__text--address`).textContent = `${mocks[numberOfMock].offer.address}`;
+  newCard.querySelector(`.popup__text--price`).textContent = `${mocks[numberOfMock].offer.price}₽/ночь`;
+  newCard.querySelector(`.popup__type`).textContent = `${TYPES_TRANSLATE[mocks[numberOfMock].offer.type]}`;
+  newCard.querySelector(`.popup__text--capacity`).textContent = `${mocks[numberOfMock].offer.rooms} комнаты для ${mocks[0].offer.guests} гостей`;
+  newCard.querySelector(`.popup__text--time`).textContent = `Заезд после ${mocks[numberOfMock].offer.checkin}, выезд до ${mocks[0].offer.checkout}`;
   const popupFeatures = Array.from(newCard.querySelectorAll(`.popup__feature`));
   const popupFeaturesContainer = newCard.querySelector(`.popup__features`);
-  const availableFeatures = popupFeatures.filter((elem) => !mocks[numberOfCard].offer.features.some((str) => elem.className.includes(`--${str}`)));
-  availableFeatures.forEach((feature) => popupFeaturesContainer.removeChild(feature));
+  const notAvailableFeatures = popupFeatures.filter((elem) => !mocks[numberOfMock].offer.features.some((str) => elem.className.includes(`--${str}`)));
+  notAvailableFeatures.forEach((feature) => popupFeaturesContainer.removeChild(feature));
+  newCard.querySelector(`.popup__description`).textContent = `${mocks[numberOfMock].offer.description}`;
+  const photoContainer = newCard.querySelector(`.popup__photos`);
+  const imgPhoto = photoContainer.querySelector(`img`);
+  const photos = mocks[numberOfMock].offer.photos;
+  for (let i = 0; i < photos.length - 1; i++) {
+    const newImg = imgPhoto.cloneNode(true);
+    photoContainer.appendChild(newImg);
+  }
+  const imgPhotos = Array.from(photoContainer.querySelectorAll(`img`));
+  for (let i = 0; i < imgPhotos.length; i++) {
+    imgPhotos[i].src = `${photos[i]}`;
+  }
   return newCard;
 };
 const renderCards = () => {
   const fragment = document.createDocumentFragment();
-  fragment.appendChild(createCards(NUMBER_OF_CARD));
+  fragment.appendChild(createCard(NUMBER_OF_CARD));
   cardsContainer.insertBefore(fragment, document.querySelector(`.map__filters-container`));
 };
 renderCards();
