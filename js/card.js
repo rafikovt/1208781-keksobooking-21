dcf3defdcbf607814ba26d28ebfb443aad286baa
Timur <rafegg21@gmail.com>
2020-10-31 18:37:19 +0500

    Добавил деструктуризацию в card.js

--- js/card.js
+++ js/card.js
@@ -8,35 +8,36 @@ const TYPES = {
 const cardsContainer = document.querySelector(`.map`);
 const cardTemplate = document.querySelector(`#card`).content.querySelector(`.map__card`);
 
-const createCardTemplate = (mock) => {
+const createCardTemplate = (cardData) => {
+  const {author: {avatar}, offer: {title, address, price, type, rooms, guests,
+    checkin, checkout, description, features, photos}} = cardData;
   const newCard = cardTemplate.cloneNode(true);
-  const avatar = newCard.querySelector(`.popup__avatar`);
-  const title = newCard.querySelector(`.popup__title`);
-  const address = newCard.querySelector(`.popup__text--address`);
-  const price = newCard.querySelector(`.popup__text--price`);
-  const type = newCard.querySelector(`.popup__type`);
+  const avatarElement = newCard.querySelector(`.popup__avatar`);
+  const titleElement = newCard.querySelector(`.popup__title`);
+  const addressElement = newCard.querySelector(`.popup__text--address`);
+  const priceElement = newCard.querySelector(`.popup__text--price`);
+  const typeElement = newCard.querySelector(`.popup__type`);
   const capacity = newCard.querySelector(`.popup__text--capacity`);
   const time = newCard.querySelector(`.popup__text--time`);
   const popupFeatures = newCard.querySelectorAll(`.popup__feature`);
   const popupFeaturesContainer = newCard.querySelector(`.popup__features`);
-  const description = newCard.querySelector(`.popup__description`);
-  avatar.src = mock.author.avatar ? mock.author.avatar : avatar.remove();
-  title.textContent = mock.offer.title ? mock.offer.title : title.remove();
-  address.textContent = mock.offer.address ? mock.offer.address : address.remove();
-  price.textContent = mock.offer.price ? `${mock.offer.price}₽/ночь` : price.remove();
-  type.textContent = mock.offer.type ? TYPES[mock.offer.type] : type.remove();
-  capacity.textContent = (mock.offer.rooms && mock.offer.guests) !== undefined ? `${mock.offer.rooms} комнаты для ${mock.offer.guests} гостей` : capacity.remove();
-  time.textContent = (mock.offer.checkin && mock.offer.checkout) ? `Заезд после ${mock.offer.checkin}, выезд до ${mock.offer.checkout}` : time.remove();
-  if (mock.offer.features.length) {
-    const notAvailableFeatures = [...popupFeatures].filter((elem) => !mock.offer.features.some((str) => elem.className.includes(`--${str}`)));
+  const descriptionElement = newCard.querySelector(`.popup__description`);
+  avatarElement.src = avatar ? avatar : avatarElement.remove();
+  titleElement.textContent = title ? title : titleElement.remove();
+  addressElement.textContent = address ? address : addressElement.remove();
+  priceElement.textContent = price ? `${price}₽/ночь` : priceElement.remove();
+  typeElement.textContent = type ? TYPES[type] : typeElement.remove();
+  capacity.textContent = (rooms && guests) !== undefined ? `${rooms} комнаты для ${guests} гостей` : capacity.remove();
+  time.textContent = (checkin && checkout) ? `Заезд после ${checkin}, выезд до ${checkout}` : time.remove();
+  if (cardData.offer.features.length) {
+    const notAvailableFeatures = [...popupFeatures].filter((elem) => !features.some((str) => elem.className.includes(`--${str}`)));
     notAvailableFeatures.forEach((feature) => popupFeaturesContainer.removeChild(feature));
   } else {
     popupFeaturesContainer.remove();
   }
-  description.textContent = mock.offer.description ? mock.offer.description : description.remove();
+  descriptionElement.textContent = description ? description : descriptionElement.remove();
   const photoContainer = newCard.querySelector(`.popup__photos`);
   const imgPhoto = photoContainer.querySelector(`img`);
-  const photos = mock.offer.photos;
   photoContainer.innerHTML = ``;
   if (photos.length) {
     for (let i = 0; i < photos.length; i++) {
